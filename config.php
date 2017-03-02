65c0fcec5426f61c3ee54a810d05987408c0076e
Twist-tracer <twist.tracer@gmail.com>
2017-03-02 19:59:55 +0300

    update config

--- config.php
+++ config.php
@@ -12,6 +12,11 @@ return [
 		'subdomain' => 'amouser',
 		'user_login' => 'amouser@mail.ru',
 		'user_hash' => '860b925be8cccfd9a52c6f3773a67083',
-		'cookie_path' => __DIR__.DIRECTORY_SEPARATOR.'runtime'.DIRECTORY_SEPARATOR.'cookies'
+		'cookie_path' => __DIR__.DIRECTORY_SEPARATOR.'runtime'.DIRECTORY_SEPARATOR.'cookies',
+
+		'limit_rows' => 300
+	],
+	'main' => [
+		'double_tag' => 'double'
 	]
 ];
