ec1a237c7f9d281e474517f216a3546665498e8e
Sam <twist.tracer@gmail.com>
2018-06-07 19:03:50 +0300

    Removed config

--- config.php
+++ config.php
@@ -9,9 +9,9 @@ return [
 	],
 	'api' => [
 		'host' => 'amocrm.ru',
-		'subdomain' => 'amouser',
-		'user_login' => 'amouser@mail.ru',
-		'user_hash' => '860b925be8cccfd9a52c6f3773a67083',
+		'subdomain' => '',
+		'user_login' => '',
+		'user_hash' => '',
 		'cookie_path' => __DIR__.DIRECTORY_SEPARATOR.'runtime'.DIRECTORY_SEPARATOR.'cookies',
 
 		'limit_rows' => 300
